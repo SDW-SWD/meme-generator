706956a5a069d24c9ac87e4e29f6486ddbcd33f6
Sehan Dhaneera Wickramsauriya <wickramsuriyashehan@gmail.com>
2024-02-08 12:54:27 +0530

    api updated with async function

--- vite-memeGenerator/src/components/content.jsx
+++ vite-memeGenerator/src/components/content.jsx
@@ -11,9 +11,12 @@ function Content() {
     const[allMemes , setAllMemes] = React.useState([])
 
     React.useEffect(() =>{
-        fetch("https://api.imgflip.com/get_memes")
-            .then(res => res.json())
-            .then(data => setAllMemes(data.data.memes))
+        async function get_memes() {
+            const res =await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        get_memes()
     }, [])
 
     function gtMemeImage() {
